f057b28c48bebf24306ee7e6b5a51ecfaa63f5c2
Anchit Lahkar <anchitlahkae0202@gmail.com>
2021-05-20 23:34:01 +0530

    5.20,21

---
+++ School_Methord.py
@@ -0,0 +1,17 @@
+import os
+
+Name = input("PLease Enter Your Name: ")
+Class = input("PLease Enter Your Class: ")
+Section = input("PLease Enter Your Section: ")
+Roll_No = input("PLease Enter Your Roll No: ")
+Address = input("PLease Enter Your Address: ")
+Phone = input("PLease Enter Your Phone: ")
+
+os.system("CLS")
+
+print("Name: ",Name)
+print("Class: ",Class)
+print("Section: ",Section)
+print("Roll No: ",Roll_No)
+print("Address: ",Address)
+print("Phone: ",Phone)
---
+++ User_Info.py
@@ -0,0 +1,28 @@
+import os
+
+user_info = {
+    "Name:": '',
+    "Class:": '',
+    "Section:": '',
+    "Roll_No:": '',
+    "Address:": '',
+    "Phone:": '',
+}
+
+user_Query =[]
+user_full_data =[]
+print(len(user_info))
+
+for i in user_info:
+    user_info[i] = input('Please Enter Your '+ i+'  ')
+    user_Query.append(i)
+    
+    value = user_info[i]
+    user_full_data.append(value)
+
+os.system("CLS")
+
+for i in range(0, len(user_info)):
+    print(user_Query[i],' ',user_full_data[i])
+
+input("\n\nExit")
